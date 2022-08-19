3e9b70511272af83f513c9e2e9dfe80d8dc9b98d
jess <jcottrill1123@gmail.com>
2022-08-19 16:36:05 -0400

    populated lists

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLotSimulator
+{
+    internal class Car
+    {
+        public Car()
+        {
+
+        }
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; } = 2023;
+        public string EngineNoise { get; set; } 
+        public string HonkNoise { get; set; } = "Beep! Beep!";
+        public bool IsDriveable { get; set; }
+
+        public void MakeEngineNoise(string noise)
+        {
+            Console.WriteLine($"My cars engine sounds like this {noise}");
+        }
+
+        public void MakeHonkNoise()
+        {
+            Console.WriteLine($"My cars honk sounds like this {HonkNoise}");
+        }
+        
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -6,6 +6,8 @@ namespace CarLotSimulator
     {
         static void Main(string[] args)
         {
+
+            CarLot carlot=new CarLot();
             //TODO
 
             //Create a seperate class file called Car
@@ -18,6 +20,26 @@ namespace CarLotSimulator
             //Set the properties for each of the cars
             //Call each of the methods for each car
 
+            //dot notation:
+            var instance = new Car();
+            carlot.CarList.Add(instance);
+            
+
+            instance.Make = "Kia";
+            instance.Model = "EV6";
+            instance.Year = 2022;
+            instance.IsDriveable = true;
+
+            //object initializer syntax:
+            var instance2 = new Car()
+            { Make = "Chevrolet", Model = "Camaro", EngineNoise = "loud", IsDriveable = true };
+            carlot.CarList.Add(instance2);
+
+            //custom constructor
+            var instance3 = new Car("Tesla", "Model 3", 2020);
+            carlot.CarList.Add(instance3);
+
+
             //*************BONUS*************//
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
@@ -28,6 +50,18 @@ namespace CarLotSimulator
             //It should have at least one property: a List of cars
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+
+            foreach (var item in carlot.CarList)
+            {
+                Console.WriteLine(item.Year);
+                Console.WriteLine(item.Make);
+                Console.WriteLine(item.Model);
+                Console.WriteLine();
+                item.MakeEngineNoise("vroom");
+                item.MakeHonkNoise();
+            }
+
+
         }
     }
 }
